6b674b76ec34feaaf896b19a561bc7d24a7ab31c
Alpha Olomi <alphaolomi@gmail.com>
2021-04-23 02:24:30 +0300

    feat: add support for persitant sessions

--- examples/simple2.php
+++ examples/simple2.php
@@ -7,10 +7,13 @@ use Openpesa\SDK\Tests\Fixture;
 $f = new Pesa([
     'api_key' => '',
     'public_key' => Fixture::$publicKey,
+    'country' => 'TZN',
+    'currency' => 'TZS',
+    'service_provider_code' => '000000',
 ]);
 
 
 // $t = $f->getSession();
 
-$t = $f->c2b(Fixture::data());
+$t = $f->c2b(Fixture::dataMin());
 var_dump($t);
